360d9de4819b6237586c95c5320fead062d24101
engolos <enginyilmaz_fb@hotmail.com>
2020-08-14 07:37:57 +0300

    Add project files.

---
+++ AuthorApi/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAuthors")]
+        public IActionResult GetAuthors()
+        {
+            List<string> Books = new List<string>
+            {
+             "Author1","Author1","Author1"
+            };
+            return Ok(Books);
+        }
+    }
+}
---
+++ BookApi/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetBooks")]
+        public IActionResult GetBooks()
+        {
+            List<string> Books = new List<string>
+            {
+             "Book1","Book2","Book3"
+            };
+            return Ok(Books);
+        }
+    }
+}
